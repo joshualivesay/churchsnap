1c44421baec413b38926699c228417c60b883a4e
joshualivesay <joshualivesay@gmail.com>
2014-06-04 12:49:24 -0400

    Twilio Integration

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Myapp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
 root to: 'home#index'
+post 'twilio/voice' => 'twilio#voice'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
